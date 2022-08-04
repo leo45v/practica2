dc66ddd80dc3e0404121ada0aac9eff4b2de0066
Leonardo <52729066+leo45v@users.noreply.github.com>
2022-08-04 11:11:54 -0400

    Update environment.ts

--- pelis/src/environments/environment.ts
+++ pelis/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   moviesEndPointUrl: 'https://api.themoviedb.org/3',
   imagesEndPointUrl: 'https://image.tmdb.org/t/p/w300',
-  moviesEndPointKey: 'api_key=b8224a67e7dc08126aeaf209d2a6d1c9',
+  moviesEndPointKey: 'api_key=',
 };
 
 /*
